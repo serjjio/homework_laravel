661fd6031529dbdad3904b7b2e18bdbf6b4a940c
Serj <vvirus@ukr.net>
2020-05-14 15:57:04 +0300

    Add column role to users table

---
+++ database/migrations/2020_05_14_120100_add_role_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRoleUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table
+                ->enum('role', ['super_admin', 'admin', 'editor', 'visitor', 'anonymous'])
+                ->default('anonymous')
+                ->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+}
